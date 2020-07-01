b124732a94bc6bc03d8291adc517ec3a661f62c0
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2020-07-01 23:49:05 +0300

    Initial commit

---
+++ src/test/java/com/shchuko/corporatemessenger/CorporateMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shchuko.corporatemessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
