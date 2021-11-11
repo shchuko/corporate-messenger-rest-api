4a2a88f35be26a960fb80b20faa4ebaa873dda0b
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-11-11 20:39:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Corporate messenger REST API 
---
+++ src/main/java/com/shchuko/corporatemessenger/CorporateMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.shchuko.corporatemessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateMessengerApplication.class, args);
+	}
+
+}
